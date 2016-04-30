7afe393a7e666dbc0ef86d6fba662a55e4f8c3a0
CG <heartlandergames@gmail.com>
2016-04-30 17:27:10 -0500

    Initial Commit of Pre-made Files
    Adding in the stuff I've already made to the repo

---
+++ VR Something/Assets/EndZone.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+import UnityEngine.UI;
+
+var points : float;
+
+var text : Text;
+
+var gameOver : boolean;
+function Start () {
+
+}
+
+function LateUpdate () {
+    text.text = "POINTS: " + points;
+    if(gameOver)
+    {
+        text.text = "GAMEOVER" + "  " + points;
+    }
+}
+
+function OnTriggerEnter(col : Collider)
+    {
+        if(col.gameObject.tag == "ITEM")
+        {
+            points++;
+            Destroy(col.gameObject);
+        }
+        if(col.gameObject.tag == "Player")
+        {
+            gameOver = true;
+            Time.timeScale = 0;
+        }
+    }
---
+++ VR Something/Assets/ItemSpawn.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+import System.Collections.Generic;
+
+var itemList : List.<GameObject> = new List.<GameObject>();
+
+var timer : float;
+
+var interval : float;
+
+var itemHolder : GameObject;
+
+function Start () {
+    Application.targetFrameRate = 30;
+}
+
+function Update () {
+    Timer();
+    Spawn();
+}
+
+
+function Timer()
+{
+    timer += Time.deltaTime;
+}
+
+function Spawn()
+{
+    if(timer >= interval)
+    {
+        var item : GameObject = Instantiate(itemList[Random.Range(0,itemList.Count-1)], transform.position, transform.rotation);
+        item.transform.Rotate(Vector3(Random.Range(0,360),Random.Range(0,360),Random.Range(0,360)));
+
+        if(item.GetComponent(Rigidbody) == null)
+        {
+            item.AddComponent(Rigidbody);
+        }
+        item.AddComponent(ItemTumble);
+        item.GetComponent(Rigidbody).drag = 0;
+        item.tag = "ITEM";
+        item.transform.SetParent(itemHolder.transform);
+        timer = 0;
+
+    }
+}
